e95ad9fb99ea582704a6c77cad8bf675db76c529
Tamerlan Zhabassov <zhbssv@mail.ru>
2024-01-18 17:50:40 +0600

    init commit

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# competitive_programming
+# Competitive programming
+
+## Two valuable topics
+    1 design of algorithms
+    2 implementation of algorithms
+
+        
---
+++ src/test/java/kz/algorithms/competitive_programming/CompetitiveProgrammingApplicationTests.java
@@ -0,0 +1,11 @@
+package kz.algorithms.competitive_programming;
+
+import org.junit.jupiter.api.Test;
+
+class CompetitiveProgrammingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
